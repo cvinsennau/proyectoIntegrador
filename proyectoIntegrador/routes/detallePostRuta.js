411986f39596fa41e336d58394ece344bcb3f19a
davidbaastidas <dbastidasvelez@udesa.edu.ar>
2020-09-27 02:09:26 -0300

    uni varios hrefs, corregi detalles en los headers, hice el css de resultados y de login

--- proyectoIntegrador/routes/detallePostRuta.js
+++ proyectoIntegrador/routes/detallePostRuta.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 let controller = require('../controllers/detallePostController')
 
-router.get('/',controller.index)
+router.get('/', controller.index)
 
 module.exports = router;
