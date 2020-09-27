f5421f9268135ec661118fa3010898846e3c11af
davidbaastidas <dbastidasvelez@udesa.edu.ar>
2020-09-27 16:46:51 -0300

    hice que funcionaran los forms de login y registro

--- proyectoIntegrador/routes/miPerfilRuta.js
+++ proyectoIntegrador/routes/miPerfilRuta.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 let controller = require('../controllers/miPerfilController')
 
-router.get('/',controller.index)
+router.get('/', controller.index)
 
 module.exports = router;
