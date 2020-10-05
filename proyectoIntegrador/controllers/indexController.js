e7199283e0ceb449dbd14a05660b7746a4eb95b5
cvinsennau <cande.vinsennau@gmail.com>
2020-10-05 11:50:05 -0300

    sequelize

--- proyectoIntegrador/controllers/indexController.js
+++ proyectoIntegrador/controllers/indexController.js
@@ -1,3 +1,8 @@
+//let db = require('../database/models');
+//let sequelize = db.sequelize
+// conectar con la base de datos
+
+
 let controller = {
     index: function(req,res){
         return res.render('index');
