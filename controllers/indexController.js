0264312970f3ca7b6fd018859ce94a87d031ebba
davidbaastidas <dbastidasvelez@udesa.edu.ar>
2020-11-03 19:18:05 -0300

    Modele algunas cosas de la base de datos y empece con HOME/INDEX

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,29 @@
-//let db = require('../database/models');
-//let sequelize = db.sequelize
-// conectar con la base de datos
+const db = require('../database/models');
+const post = db.Post;
+const user = db.User;
+
+const op = db.Sequelize.Op;
 
 
 let controller = {
     index: function(req,res){
-        return res.render('index');
+        post.findAll()
+        .then(function(resultados){
+            return res.render('index', {resultados});
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    },
+    show: function(req,res){
+        let primaryKey = req.params.id;
+        user.findByPk(primaryKey)
+            .then(function(user){
+                return res.render('index', {user});
+            })
+            .catch(function(error){
+                console.log(error);
+            })
     }
 
 }
---
+++ database/models/Post.js
@@ -0,0 +1,37 @@
+module.exports = function (sequelize, dataTypes){
+
+    let alias = "Post" //nombre para identificar al modelo - se busca como nombre en la tabla en plural en la base de datos.
+
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        id_user: {
+            type: dataTypes.INTEGER,
+            allowNull: false
+        },
+        image_user: {
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+        text_post: {
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+        date_post: {
+            type: dataTypes.DATE,
+            allowNull: false
+        }
+    } //nombre de las columnas de la tabla
+
+
+    let config = {
+        
+    }
+
+    const Post = sequelize.define(alias, cols, config);
+
+    return Post
+}
