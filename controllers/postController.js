48f066bed7ac79c139cd5e87523ce6992293d923
tomasgiamp99 <tgiampaoletti@udesa.edu.ar>
2020-11-11 01:59:11 -0300

    me voy a dormir

--- controllers/postController.js
+++ controllers/postController.js
@@ -29,24 +29,6 @@ let controller = {
                 console.log(error);
             })
     },
-    // resultadoUsuarios: (req,res)=>{ // Buscador de Usuarios
-    //     var busqueda = req.query.nombreUsuario // Saca de la url lo que el usuario escribio en el buscador
-    //     user.findAll({ // buscame en la base de datos, tabla de usuarios ..
-    //       where: [
-    //         {email: {[op.like]:"%"+ busqueda + "%"}},
-    //       ]
-    //         [OP.or]:[
-    //           {name:{[OP.like]: "%"+ busqueda + "%"}},  // por nombre O por email, si existe con cualquiera de las dos
-    //         ]
-    //     })
-    //     .then (function(resultado){ // resultado es lo que trae de la bd el find all
-    //       console.log(resultado)
-    //       if (resultado.length >0 ){
-    //       res.render("resultados", {resultado:resultado}) }// si hay un resultado mandame a la vista (donde voy a ver todos os usuarios que matchean con mi busqueda)
-    //       else{
-    //         console.log('No hay resultados')
-    //         //res.redirect ('/playit/users') // sino hay resultado volveme a cargar la pag asi busco otra cosa
-    //       }})
-    }
+}
 
 module.exports = controller;
