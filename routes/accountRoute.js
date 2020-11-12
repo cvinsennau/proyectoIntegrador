b397f0da6f27f1a4aba08f24006086edbb86f3b0
tomasgiamp99 <tgiampaoletti@udesa.edu.ar>
2020-11-12 18:59:23 -0300

    question arreglado jeje

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -6,7 +6,7 @@ let questionController = require('../controllers/questionController')
 router.get('/login',accountController.login);
 router.post('/login', accountController.loginProcess);
 router.get('/recover',accountController.recover);
-router.get('/signup',accountController.register);
+//router.get('/signup',accountController.register);
 router.post('/signup', accountController.store);
 router.get('/signup', questionController.index); 
 router.post('/logout', accountController.logout)
